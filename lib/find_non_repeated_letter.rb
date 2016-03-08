ee36961aacb371199b898ce6e76d9bfcbb7e7a26
webbedfooty <patrickwiechert@yahoo.com>
2016-03-08 01:41:30 +0000

    I'm Done!

--- lib/find_non_repeated_letter.rb
+++ lib/find_non_repeated_letter.rb
@@ -8,7 +8,47 @@
 #   nil (since there does not exist a non-repeated letter)
 
 
+
+
 def find_first_non_repeated_letter(str)
-  #write your method here
-  return true
+find_first_non_repeated_letter = String.new
+
+found = false
+found_it = String.new
+here_you_go = String.new
+
+x = str.length - 2
+y = 0
+a = str[y]
+b = str[y+1]
+c = str[y+2]
+found_it = nil
+
+if a != b
+  found_it = a
+  find_first_non_repeated_letter = found_it
+else
+
+  while x > -2 && found == false
+
+        a = str[y-1]
+        b = str[y]
+        c = str[y+1]
+        found = false
+        if a != b && b != c
+          found_it = b
+          found = true
+          else
+            y = y + 1
+            x = x - 1
+  end
+find_first_non_repeated_letter = found_it
 end
+end
+return find_first_non_repeated_letter
+end
+#  puts "Give me a string of letters"
+#  puts " "
+#  str = gets.chomp
+
+# puts here_you_go = find_first_non_repeated_letter(str)
